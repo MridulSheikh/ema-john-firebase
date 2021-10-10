5a20740e9af3c67a79cb4344afa2794ffdac6257
MridulSheikh <prince9nazir@gmail.com>
2021-10-10 18:24:24 +0600

    added firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDkQmzWLI6LEfMTLINuKbk5UTt3L3kzTQ",
+    authDomain: "ema-john-69a54.firebaseapp.com",
+    projectId: "ema-john-69a54",
+    storageBucket: "ema-john-69a54.appspot.com",
+    messagingSenderId: "790214772269",
+    appId: "1:790214772269:web:657671b3d97956cf0cdaa4"
+  };
+export default firebaseConfig;  
